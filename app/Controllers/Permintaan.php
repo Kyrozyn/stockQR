c3cda98eb03bed98dd847d5f47b28a285d86e60c
Andreas <arizkirizaldi@email.unikom.ac.id>
2020-09-07 22:52:35 +0700

    fix permintaan

--- app/Controllers/Permintaan.php
+++ app/Controllers/Permintaan.php
@@ -20,11 +20,11 @@ class Permintaan extends BaseController
         $crud->setTable('permintaans');
         $crud->unsetPrint();
         $crud->unsetExport();
-        if (!checkIsAdmin()) {
-            $crud->unsetAdd();
-            $crud->unsetDelete();
-            $crud->unsetEdit();
-        }
+//        if (!checkIsAdmin()) {
+//            $crud->unsetAdd();
+//            $crud->unsetDelete();
+//            $crud->unsetEdit();
+//        }
 //        $crud->uniqueFields(['ID_Permintaan']);
         $crud->requiredFields(['Tanggal', 'Nama_Barang', 'Jumlah_Barang', 'Nama_Pemohon']);
         $output = $crud->render();
