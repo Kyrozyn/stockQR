99afbc9e3bcff451ef9aa1e422c34e7fe8a389b7
Andreas <arizkirizaldi@email.unikom.ac.id>
2020-09-17 01:28:52 +0700

    logo

--- app/Config/App.php
+++ app/Config/App.php
@@ -21,7 +21,7 @@ class App extends BaseConfig
     | environments.
     |
     */
-    public $baseURL = 'https://7e2c1feabbb2.ap.ngrok.io/';
+    public $baseURL = 'https://qrcodebbg.herokuapp.com/';
 
     /*
     |--------------------------------------------------------------------------
